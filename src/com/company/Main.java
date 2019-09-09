776034788cf2697efc54bb0b3b3db131a346dcab
ericagleaton <EricaGleaton@comcast.net>
2019-09-09 14:27:13 -0400

    EJG 09/09/2019,iffy Loopy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,31 @@
 package com.company;
+/*
+Loopy
+Create ONE string variable to store the output of 5 words that a user will enter at random.
 
+Iffy
+Set a boolean value called redEyes.
+
+The value will depend on the value entered by the user for the question "Are your eyes red?"
+
+
+Create an application that will ask,
+
+"Are your eyes red?"
+
+If the response is 'y', Or 'yes' (regardless of case), then your application should print out:
+"Get some sleep!"
+
+
+Otherwise your application should print out "You look great!"
+
+Use the value of redEyes to determine whether the user should be complimented or not.
+
+Iffy Loop
+Make sure after the application asks the question the first time, you get a prompt asking,
+"Do you want to try again?"
+Your application should continue until the user enters 'n' as an answer.
+*/
 import java.util.Scanner;
 
 public class Main {
